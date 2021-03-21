909332fdcf8dadea1b36b84c37eb8e93d12a088e
Luis Fernando <fernandocf_j@outlook.com>
2021-03-20 19:52:04 -0600

    crear y listar ticket

--- controller/ticket.php
+++ controller/ticket.php
@@ -6,9 +6,30 @@
     switch($_GET["options"]){
         case "insert":
             $ticket->insertTicket($_POST["user_id"],$_POST["categori_id"],$_POST["ticket_title"],$_POST["ticket_description"]);
-            
-            
         break;
+        
+        case "listByUser":
+            $datos =  $ticket->listarByUser($_POST["user_id"]);
+            $data  = Array();
+            
+            foreach($datos as $row ){
+                $sub_array = array();
+                $sub_array[] = $row["ticket_id"];
+                $sub_array[] = $row["categori_name"];
+                $sub_array[] = $row["ticket_title"];
+                $sub_array[] = '<button type="button" onClick="ver('.$row["ticket_id"].');"  id="'.$row["ticket_id"].'" class="btn btn-inline btn-primary btn-sm ladda-button"><i class="fa fa-eye"></i></button>';
+                $sub_array[] = date("d/m/Y H:i:s", strtotime($row["date_create"]));
+                $data[] = $sub_array;
+            }
+            $results = array(
+                "sEcho"=>1,
+                "iTotalRecords"=>count($data),
+                "iTotalDisplayRecords"=>count($data),
+                "aaData"=>$data);
+            echo json_encode($results);
+
+        break;
+
     }
 
 ?>
--- models/ticket.php
+++ models/ticket.php
@@ -1,20 +1,47 @@
-<?php 
+<?php
 
-    class Ticket extends Connect{
+class Ticket extends Connect
+{
 
-        public function insertTicket($user_id,$categori_id,$ticket_title,$ticket_description){
-            $connect = parent::Conexion();
-            parent::set_names();
+    public function insertTicket($user_id, $categori_id, $ticket_title, $ticket_description)
+    {
+        $connect = parent::Conexion();
+        parent::set_names();
 
-            $sql = "INSERT INTO tm_ticket (ticket_id, user_id, categori_id, ticket_title, ticket_description, status) VALUES (NULL, ?, ?, ?, ?, '1');";
-            $sql = $connect->prepare($sql);
-            $sql -> bindValue(1,$user_id);
-            $sql -> bindValue(2,$categori_id);
-            $sql -> bindValue(3,$ticket_title);
-            $sql -> bindValue(4,$ticket_description);
+        $sql = "INSERT INTO tm_ticket (ticket_id, user_id, categori_id, ticket_title, ticket_description,date_create, status) 
+        VALUES (NULL, ?, ?, ?, ?, now(), '1');";
+        $sql = $connect->prepare($sql);
+        $sql->bindValue(1, $user_id);
+        $sql->bindValue(2, $categori_id);
+        $sql->bindValue(3, $ticket_title);
+        $sql->bindValue(4, $ticket_description);
 
-            $sql->execute();
-            return $resultado = $sql->fetchAll();
-        }
+        $sql->execute();
+        return $resultado = $sql->fetchAll();
     }
-?>
+
+    public function listarByUser($user_id)
+    {
+        $conectar = parent::conexion();
+        parent::set_names();
+        $sql = "SELECT 
+            tm_ticket.ticket_id, 
+            tm_ticket.user_id, 
+            tm_ticket.categori_id, 
+            tm_ticket.ticket_title, 
+            tm_ticket.ticket_description,
+            tm_ticket.date_create, 
+            tm_ticket.status, 
+            tm_usuario.user_name, 
+            tm_categoria.categori_name
+        FROM tm_ticket 
+        INNER join tm_categoria on tm_ticket.categori_id = tm_categoria.categori_id 
+        INNER join tm_usuario on tm_ticket.user_id = tm_usuario.user_id
+        WHERE tm_ticket.status = 1 
+        AND tm_usuario.user_id=?";
+        $sql = $conectar->prepare($sql);
+        $sql->bindValue(1, $user_id);
+        $sql->execute();
+        return $resultado = $sql->fetchAll();
+    }
+}
